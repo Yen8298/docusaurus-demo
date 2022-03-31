b03590f5c033267d6eed3c0cc97f8ff4521c7b1c
Yen8298 <yen.nguyen@katalon.com>
2022-03-31 14:46:06 +0700

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -233,25 +233,4 @@ const config = {
       hideableSidebar: true,
     }),
 
-  plugins: [
-    'docusaurus-plugin-sass',
-    'docusaurus-plugin-image-zoom',
-    [
-      '@docusaurus/plugin-client-redirects',
-      // highlight-start
-      {
-        fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
-        redirects: [
-          // Redirect from multiple old paths to the new path
-          {
-            to: '/docs/katalon-studio-enterprise/release-notes/version-8.x',
-            from: ['/katalon-studio/new/version-8x.html'],
-          },
-        ],
-      },
-      // highlight-end
-    ],
-  ],
-};
-
 module.exports = config;
