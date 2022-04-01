aee7372ae30e162108fcac34f0f2bd47d08485bd
Yen8298 <yen.nguyen@katalon.com>
2022-04-01 10:36:05 +0700

    Update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -240,7 +240,6 @@ const config = {
       '@docusaurus/plugin-client-redirects',
       // highlight-start
       {
-        fromExtensions: ['html', 'htm'], // /myPage.html -> /myPage
         redirects: [
           // Redirect from multiple old paths to the new path
           {
@@ -248,15 +247,6 @@ const config = {
             from: '/katalon-studio/new/version-8x.html',
           },
         ],
-        createRedirects(existingPath) {
-          if (existingPath.includes('.html')) {
-            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
-            return [
-              existingPath.replace('', '.html'),
-            ];
-          }
-          return undefined; // Return a falsy value: no redirect created
-        },
       },
       // highlight-end
     ],
